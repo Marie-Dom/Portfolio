630be4fe18964d1485c15a385a029a468b4898ae
Marie-Dominique JAOUEN <md.friot@gmail.com>
2023-12-11 20:35:37 +0100

    Modifying and deleting bad pictures, adding skills and resume in portfolio

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -23,3 +23,21 @@ function linkAction() {
   // Quand on clique sur chaque lien nav__link, nous supprimons la class "show-menu"
   navMenu.classList.remove("show-menu");
 }
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// ACCORDION SKILLS
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent - length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
