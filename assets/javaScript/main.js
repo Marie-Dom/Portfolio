b66c5395f7639ef50726e5b9a2ec4804442cf558
Marie-Dominique JAOUEN <md.friot@gmail.com>
2023-12-12 23:55:23 +0100

    Adding portfolio folder and modifying html and css files

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -41,3 +41,19 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
+// PORTFOLIO SWIPER
+let swiperPortfolio = new Swiper(".portfolio-container", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
